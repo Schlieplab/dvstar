a07645887cfa755922d31fd14d50bf0ae8eebcf6
Joel Gustafsson <me@joelgustafsson.com>
2023-10-06 13:24:26 +0200

    Remove part of CLI11 tests before building artifacts

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# apptainer can't handle the circular submodule in CLI11 tests
+rm -rf submodules/CLI11/tests/mesonTest/subprojects/CLI11
+
+mkdir -p artifacts
+
 sudo apptainer build --force vlmc-from-kmers.sif vlmc-from-kmers.def
 
 apptainer exec --bind $PWD/artifacts/:/artifacts vlmc-from-kmers.sif sh -c 'cp /vlmc-from-kmers/build/dvstar /artifacts'
